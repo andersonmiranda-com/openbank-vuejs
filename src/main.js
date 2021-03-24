b88ae1be8053ff6c8fbdb8946d97a2b5a2ad36f7
Anderson Miranda <me@andersonmiranda.com>
2021-03-24 19:14:12 +0100

    fixed bootstrap colors

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import i18n from "./locales/i18n";
 
 createApp(App)
